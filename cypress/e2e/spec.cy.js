15ac9524130526235d612412e2b806b6332bbc75
tepokev <tepokev@gmail.com>
2024-01-19 10:02:26 -0600

    dmm flow e2e test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -31,12 +31,11 @@ describe('Load page', () => {
     cy.get('[id="type"]').select('2')
     cy.get('[name="Submit"]').should('exist').click({multiple: true})
   })
-  it('Logout', () => {
-    cy.visit('http://localhost:8080/LibreClinica/MainMenu')
-    cy.url().should('include', 'http://localhost:8080/LibreClinica/MainMenu')
-    cy.get('[id="logout"]').should('exist').click()
-    cy.url().should('include', 'http://localhost:8080/LibreClinica/pages/login/login')
+  it('oAuth login', () => {
+    cy.visit('http://localhost:8080/LibreClinica/pages/login/login')
     cy.contains('Login')
+    cy.contains('OAuth Login').click()
+    cy.url().should('include', 'http://localhost:8080/LibreClinica/CreateUserAccount')
   })
 
   /*it('visit dmm', () => {
