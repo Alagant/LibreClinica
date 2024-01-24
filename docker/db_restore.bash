5e29664dd86cf5f64505f83e43fe5026552b006b
Lucioric2000 <lucioric2000@hotmail.com>
2024-01-23 23:30:34 -0600

    Added code for database restoral from a backup

--- docker/db_restore.bash
+++ docker/db_restore.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Script to backup LibreClinica database
-echo dn "$(dirname "$0")"
-backupfilename=libreclinica_$(date -Iseconds).sql
-echo Backing up the database to /home/azureuser/dbbackup/$backupfilename
-cd "$(dirname "$0")"/.. && /usr/bin/docker compose run postgres pg_dump -d libreclinica -U clinica -h postgres>/home/azureuser/dbbackup/$backupfilename
+#echo dn "$(dirname "$0")"
+backupfilename=/docker-folder/libreclinica_2023-12-24T01_20_23-06_00.sql
+echo Renewing database
+cd "$(dirname "$0")"/.. && /usr/bin/docker compose run postgres psql -d postgres -U clinica -h postgres -c "DROP DATABASE libreclinica IF EXISTS; CREATE DATABASE libreclinica;"
+echo Restoring database from $backupfilename
+cd "$(dirname "$0")"/.. && /usr/bin/docker compose run postgres psql -d clinica -U clinica -h postgres -f $backupfilename
