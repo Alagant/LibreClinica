babb36de23c58c7a16c22aab02239a4d638d8161
Lucioric2000 <lucioric2000@hotmail.com>
2024-01-25 10:17:34 -0600

    Fixed db_backup.bash

--- docker/db_backup.bash
+++ docker/db_backup.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Script to backup LibreClinica database
 dir="$(dirname "$0")"
-dir=$HOME/dbbackup
+backup_dir=$HOME/dbbackup
 if [ $1 ];
 then
   backupfilename=$1
@@ -10,4 +10,4 @@ else
 fi
 
 echo Backing up the database to /home/azureuser/dbbackup/$backupfilename
-cd ${dir}/.. && /usr/bin/docker compose run postgres pg_dump -d libreclinica -U clinica -h postgres>/home/azureuser/dbbackup/$backupfilename
+cd ${dir}/.. && /usr/bin/docker compose run postgres pg_dump -d libreclinica -U clinica -h postgres>${backup_dir}/$backupfilename
