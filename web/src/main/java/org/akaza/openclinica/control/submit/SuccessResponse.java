b2caafb150b229b29d3fb465f059a062e7a0f9f9
tepokev <tepokev@gmail.com>
2024-02-02 05:34:21 -0600

    fixed blank page after insert EN data

--- web/src/main/java/org/akaza/openclinica/control/submit/SuccessResponse.java
+++ web/src/main/java/org/akaza/openclinica/control/submit/SuccessResponse.java
@@ -1,16 +1,26 @@
 package org.akaza.openclinica.control.submit;
 import com.fasterxml.jackson.annotation.JsonProperty;
 public class SuccessResponse {
-    @JsonProperty("pid")
+    @JsonProperty(value = "id", required = false)
+    private int id;
+    @JsonProperty(value = "pid", required = false)
     private String pid;
+    @JsonProperty(value = "success", required = false)
     private String success;
+    @JsonProperty(value = "libreclinica_id", required = false)
+    private String libreclinicaId;
+    @JsonProperty(value = "schedule", required = false)
+    private Schedule schedule;
 
     public SuccessResponse() {
     }
 
-    public SuccessResponse(String pid, String success) {
+    public SuccessResponse(String pid, String success, String libreclinicaId, Schedule schedule, int id) {
         this.pid = pid;
         this.success = success;
+        this.libreclinicaId = libreclinicaId;
+        this.schedule = schedule;
+        this.id = id;
     }
 
     public String getPid() {
@@ -27,5 +37,23 @@ public class SuccessResponse {
 
     public void setSuccess(String success) {
         this.success = success;
+    }
+    public String getLibreclinca_id() {
+        return libreclinicaId;
+    }
+    public void setLibreclinca_id(String libreclincaId) {
+        this.libreclinicaId = libreclincaId;
+    }
+    public Schedule getSchedule() {
+        return schedule;
+    }
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
     }
 }
