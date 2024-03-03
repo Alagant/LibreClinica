1bba693ac96a3dc2d2c89a29db7bf87d28a57c9c
tepokev <tepokev@gmail.com>
2024-03-03 14:49:35 -0600

    New subjects cannot now be assigned a CRF other than BL.

--- web/src/main/java/org/akaza/openclinica/control/submit/CreateNewStudyEventServlet.java
+++ web/src/main/java/org/akaza/openclinica/control/submit/CreateNewStudyEventServlet.java
@@ -317,10 +317,14 @@ public class CreateNewStudyEventServlet extends SecureController {
             	// add an error here, tbh
             	Validator.addError(errors, INPUT_STUDY_SUBJECT, respage.getString("must_enter_subject_ID_for_identifying"));
             }
-            if(studySubject.getSecondaryLabel().isEmpty()) {
-                Validator.addError(errors, INPUT_STUDY_SUBJECT, "Study Subject is not enrolled. Enroll subject from subject matrix.");
+            if(!"BL".equalsIgnoreCase(definition.getName())){
+                if(studySubject.getSecondaryLabel().isEmpty()) {
+                    Validator.addError(errors, INPUT_STUDY_SUBJECT, "Study Subject is not enrolled. Enroll subject from subject matrix.");
+                    if(!definition.getName().isEmpty()){
+                        Validator.addError(errors, INPUT_STUDY_EVENT_DEFINITION, "Only the BL event can be scheduled for new subjects.");
+                    }
+                }
             }
-
             if (!subjectMayReceiveStudyEvent(sm.getDataSource(), definition, studySubject)) {
                 Validator.addError(errors, INPUT_STUDY_EVENT_DEFINITION, restext.getString("not_added_since_event_not_repeating"));
             }
@@ -332,6 +336,14 @@ public class CreateNewStudyEventServlet extends SecureController {
                     int pk = fp.getInt(INPUT_STUDY_EVENT_DEFINITION_SCHEDULED[i]);
                     if (pk > 0) {
                         StudyEventDefinitionBean sedb = (StudyEventDefinitionBean) seddao.findByPK(pk);
+                        if(!"BL".equalsIgnoreCase(definition.getName())){
+                            if(studySubject.getSecondaryLabel().isEmpty()) {
+                                if(!sedb.getName().isEmpty()){
+                                    Validator.addError(errors, INPUT_STUDY_EVENT_DEFINITION_SCHEDULED[i], "Only the BL event can be scheduled for new subjects.");
+                                }
+                            }
+
+                        }
                         logger.debug("scheduled def:" + pk + " " + INPUT_STUDY_EVENT_DEFINITION_SCHEDULED[i] + " " + sedb.getName());
                         definitionScheduleds.add(sedb);
                         scheduledDefinitionIds[i] = pk;
