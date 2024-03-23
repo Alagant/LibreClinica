06012adb7911efce1f8d37910110d3c0183e5b41
Lucioric2000 <lucioric2000@hotmail.com>
2024-03-22 20:31:05 -0600

    Added ODM Java files

--- odm/src/main/java/org/cdisc/ns/odm/v130_api/ODMcomplexTypeDefinitionStudyName.java
+++ odm/src/main/java/org/cdisc/ns/odm/v130_api/ODMcomplexTypeDefinitionStudyName.java
@@ -6,7 +6,7 @@
 //
 
 
-package org.cdisc.ns.odm.v130_api_dos;
+package org.cdisc.ns.odm.v130_api;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
